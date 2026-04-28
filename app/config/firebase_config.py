a47f35349748d98391f3a7db58c4b6bdbdc8c9df
komalparmar <komalparmar5611@gmail.com>
2026-04-28 14:28:55 +0530

    fix: Firebase credentials from env var

--- app/config/firebase_config.py
+++ app/config/firebase_config.py
@@ -6,17 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+import json
+
 if not firebase_admin._apps:
-    cred_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
-    if cred_json:
-        cred_dict = json.loads(cred_json)
-        cred = credentials.Certificate(cred_dict)
+    service_account_json = os.getenv("FIREBASE_SERVICE_ACCOUNT_JSON")
+
+    if service_account_json:
+        service_account_dict = json.loads(service_account_json)
+        cred = credentials.Certificate(service_account_dict)
     else:
         cred_path = "./app/config/ServiceAccountKey.json"
         cred = credentials.Certificate(cred_path)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-def init_firebase():
-    pass
